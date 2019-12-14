d2d957fedc7e0493d16c874361878ce33e1a29a3
李翔宇 <lixiangyu@lichendt.com>
2019-12-14 15:00:36 +0800

    修改

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">肉食品溯源系统</h2>
+          <p class="brand-info__intro">通过对视频流通中各环节关键节点筛选，实现对肉食品企业信息的完整记录以及实时溯源。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
